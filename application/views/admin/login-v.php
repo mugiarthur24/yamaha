ef3765d4959d22dba02c2637697ba82db748b5b6
Reza Rafiq MZ <rezarafiqmz@gmail.com>
2019-12-12 17:28:06 +0800

    masukian logo ke tempalte login

--- application/views/admin/login-v.php
+++ application/views/admin/login-v.php
@@ -4,13 +4,9 @@
 <head>
     <title><?php echo $title; ?></title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="#">
-    <meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="<?php echo base_url('assets/img/lembaga/'.$infopt->logo_pt) ?>">
     <meta name="author" content="#">
-    <!-- Favicon icon -->
-    <link rel="icon" href="..\files\assets\images\favicon.ico" type="image/x-icon">
     <!-- Google font--><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet">
     <!-- Required Fremwork -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/adminty/files/') ?>\bower_components\bootstrap\css\bootstrap.min.css">
@@ -50,7 +46,7 @@
                     <!-- Authentication card start -->
                         <form action="<?php echo base_url('index.php/login/proses_login/') ?>" method="post" class="md-float-material form-material">
                             <div class="text-center">
-                                <img src="..\files\assets\images\logo.png" alt="logo.png">
+                                <img src="<?php echo base_url('assets/img/lembaga/'.$infopt->logo_pt) ?>" alt="<?php echo $infopt->logo_pt; ?>">
                             </div>
                             <div class="auth-box card">
                                 <div class="card-block">
@@ -94,11 +90,8 @@
                                     <hr>
                                     <div class="row">
                                         <div class="col-md-10">
-                                            <p class="text-inverse text-left m-b-0">Thank you.</p>
-                                            <p class="text-inverse text-left"><a href="index-1.htm"><b class="f-w-600">Back to website</b></a></p>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <img src="..\files\assets\images\auth\Logo-small-bottom.png" alt="small-logo.png">
+                                            <p class="text-inverse text-left m-b-0"><?php echo $infopt->nama_info_pt; ?></p>
+                                            <p class="text-inverse text-left"><a href="index-1.htm"><b class="f-w-600"><?php echo date('Y'); ?></b></a></p>
                                         </div>
                                     </div>
                                 </div>
@@ -112,49 +105,6 @@
         </div>
         <!-- end of container-fluid -->
     </section>
-    <!-- Warning Section Starts -->
-    <!-- Older IE warning message -->
-    <!--[if lt IE 10]>
-<div class="ie-warning">
-    <h1>Warning!!</h1>
-    <p>You are using an outdated version of Internet Explorer, please upgrade <br/>to any of the following web browsers to access this website.</p>
-    <div class="iew-container">
-        <ul class="iew-download">
-            <li>
-                <a href="http://www.google.com/chrome/">
-                    <img src="../files/assets/images/browser/chrome.png" alt="Chrome">
-                    <div>Chrome</div>
-                </a>
-            </li>
-            <li>
-                <a href="https://www.mozilla.org/en-US/firefox/new/">
-                    <img src="../files/assets/images/browser/firefox.png" alt="Firefox">
-                    <div>Firefox</div>
-                </a>
-            </li>
-            <li>
-                <a href="http://www.opera.com">
-                    <img src="../files/assets/images/browser/opera.png" alt="Opera">
-                    <div>Opera</div>
-                </a>
-            </li>
-            <li>
-                <a href="https://www.apple.com/safari/">
-                    <img src="../files/assets/images/browser/safari.png" alt="Safari">
-                    <div>Safari</div>
-                </a>
-            </li>
-            <li>
-                <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
-                    <img src="../files/assets/images/browser/ie.png" alt="">
-                    <div>IE (9 & above)</div>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <p>Sorry for the inconvenience!</p>
-</div>
-<![endif]-->
     <!-- Warning Section Ends -->
     <!-- Required Jquery -->
     <script type="text/javascript" src="<?php echo base_url('assets/adminty/files/') ?>\bower_components\jquery\js\jquery.min.js"></script>
